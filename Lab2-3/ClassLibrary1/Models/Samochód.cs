73448bd6c14be3578d738f8216f8598851c4e5ca
casey <karolina.korczynska.5@gmail.com>
2023-12-06 12:57:22 +0100

    Laboratorium 1, 2-3 (file organization cleanup - no file content changes)

---
+++ Lab2-3/ClassLibrary1/Models/Samochód.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Models
+{
+    public class Samochód
+    {
+        public String marka { get;  }
+        public String model { get;  }
+        public int rokProdukcji { get; }
+
+        private double prędkość;
+        private int przebieg;
+        private Silnik stanSilnika;
+        private enum Silnik
+        {
+            ON,
+            OFF,
+            CHECK_ENGINE
+        };
+
+        public Samochód(String marka, String model, int rok)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.rokProdukcji = rok;
+            this.prędkość = 0;
+            this.przebieg = 0;
+            this.stanSilnika = Silnik.OFF;
+        }
+        public void UstawTempomat(int zadanaPredkosc)
+        {
+            SprawdzStanSilnika();
+            prędkość = zadanaPredkosc;
+
+        }
+        public void ZwiekszPredkosc()
+        {
+            SprawdzStanSilnika();
+            prędkość += 5;
+        }
+
+        public void ZmniejszPredkosc()
+        {
+            SprawdzStanSilnika();
+            prędkość -= 5;
+        }
+
+        private void SprawdzStanSilnika()
+        {
+            if (stanSilnika != Silnik.ON)
+            {
+                throw new InvalidOperationException("Silnik nie jest uruchomiony");
+            }
+        }
+        public void UruchomSilnik()
+        {
+            if (stanSilnika == Silnik.CHECK_ENGINE)
+            {
+                throw new InvalidOperationException("Nie można uruchomić silnika z powodu awarii");
+            }
+
+            stanSilnika = Silnik.ON;
+        }
+
+        public void ZatrzymajSilnik()
+        {
+            stanSilnika = Silnik.OFF;
+        }
+
+        public double PrzejedzDystans(int dystans)
+        {
+            SprawdzStanSilnika();
+
+            if (dystans < 0)
+            {
+                throw new ArgumentException("Dystans musi być nieujemny");
+            }
+
+            przebieg += dystans;
+
+            if (przebieg > 10000)
+            {
+                stanSilnika = Silnik.CHECK_ENGINE;
+                throw new InvalidOperationException("Awaria silnika (Check Engine)! Nie można kontynuować jazdy.");
+            }
+
+            return dystans / (double)prędkość;
+        }
+
+        private void NaprawSilnik()
+        {
+            stanSilnika = Silnik.ON;
+        }
+    }
+}
---
+++ Lab2-3/TestProject1/LiczTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1.Models;
+
+namespace TestProject1
+{
+    public class LiczTest
+    {
+        private Licz licz = new(0);
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void ShouldCreateObject()
+        {
+            Assert.NotNull(licz);
+        }
+
+        [Test]
+        [TestCase(2, 5, 7)]
+        [TestCase(5, 6, 11)]
+        public void AddTest(int num1, int num2, int expected)
+        {
+            licz.Add(num1);
+            licz.Add(num2);
+            Assert.That(licz.Value, Is.EqualTo(expected));
+        }
+        [Test]
+        [TestCase(2, 5, -7)]
+        [TestCase(5, 6, -11)]
+        public void SubstractTest(int num1, int num2, int expected)
+        {
+            licz.Substract(num1);
+            licz.Substract(num2);
+            Assert.That(licz.Value, Is.EqualTo(expected));
+        }
+
+
+
+    }
+}

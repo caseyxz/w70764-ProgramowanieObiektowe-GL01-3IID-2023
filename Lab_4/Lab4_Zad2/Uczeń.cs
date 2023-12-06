bcaedcbce6248f889b61d8d4a36701e6a2e1970d
casey <karolina.korczynska.5@gmail.com>
2023-12-06 12:52:24 +0100

    Laboratorium 4

---
+++ Lab_4/Lab4_Zad2/Uczeń.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Zad2
+{
+    public class Uczeń : Osoba
+    {
+        public override bool CanGoAloneToHome()
+        {
+            if (GetAge() >= 12 ) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void GetEducationInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetFullName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
